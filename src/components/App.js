b8ac08a42cfd64a8bf6b3a91caa6d9ea20597f0f
euonymus <euonymus0220@gmail.com>
2018-09-05 15:08:51 +0900

    chore

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
@@ -11,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Link to={`/sample_component`} >Component Example</Link>
         </p>
       </div>
     );
