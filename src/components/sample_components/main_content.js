4e55d71b993390c16eb156913dd723f9c787c828
euonymus <euonymus0220@gmail.com>
2018-09-05 14:59:48 +0900

    chore

--- src/components/sample_components/main_content.js
+++ src/components/sample_components/main_content.js
@@ -6,15 +6,34 @@ import { connect } from 'react-redux';
 import { selectMenu } from '../../actions/sample_component';
 
 class SampleMain extends Component {
+    state = {
+	term: ''
+    }
+
     componentWillMount() {
       this.props.selectMenu('Not Selected')
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        // this.props.onSearchTermChange(term);
     }
 
     render() {
 	return (
       <div id="content">
          <h2 className="title">{this.props.current_sample_component_data}</h2>
-         <p>main</p>
+         <p>
+            <div className="">
+                <input
+                    value = {this.state.term}
+                    onChange = {event => this.onInputChange(event.target.value)} />
+            </div>
+            <div className="">
+               <h3 className="title">This is what you typed</h3>
+	       {this.state.term}
+            </div>
+         </p>
       </div>
 	);
     }
